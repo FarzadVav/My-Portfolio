b5b085850dbf4dbf0dccd88a0c9133ff6cb3c3cc
FarzadVav <farzad.vav.work@gmail.com>
2024-05-14 17:01:37 +0330

    update: complete Header

--- components/modules/Header.tsx
+++ components/modules/Header.tsx
@@ -1,7 +1,9 @@
 "use client"
 
 import Link from "next/link"
-import { HomeIcon, LinkIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid"
+import { useEffect, useState } from "react"
+import { v4 } from "uuid"
+import { HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid"
 import { MoonIcon, ShareIcon, SunIcon } from "@heroicons/react/24/outline"
 
 const links = [
@@ -12,6 +14,18 @@ const links = [
 ]
 
 const Header = () => {
+  const [theme, setTheme] = useState<"dark" | "light">("dark")
+
+  useEffect(() => {
+    themeToggleHandler((localStorage.getItem("theme") as typeof theme | "") || "dark")
+  }, [])
+
+  const themeToggleHandler = (currentTheme: typeof theme) => {
+    setTheme(currentTheme)
+    localStorage.setItem("theme", currentTheme)
+    document.querySelector("html")?.setAttribute("data-theme", currentTheme)
+  }
+
   return (
     <header className="border-b border-solid border-neutral w-full z-40">
       <nav className="container row h-20">
@@ -22,42 +36,55 @@ const Header = () => {
         </Link>
         <div className="row h-full gap-3 mr-3">
           {links.map((link) => (
-            <Link href={link.href}>
-              <button className="btn btn-sm btn-ghost rounded-full">{link.text}</button>
+            <Link
+              key={v4()}
+              className="py-1 px-3 mr-3 transition-colors hover:text-primary"
+              href={link.href}
+            >
+              {link.text}
             </Link>
           ))}
         </div>
         <label className="input input-sm input-bordered row gap-3 mr-auto rounded-full">
           <MagnifyingGlassIcon className="icon-sm" />
-          <input type="text" className="w-36" placeholder="جستجو" />
+          <input type="text" className="w-40" placeholder="جستجو" />
         </label>
-        <button className="btn btn-ghost mr-3 rounded-none relative group hover:bg-transparent">
-          <ShareIcon tabIndex={0} className="icon" />
-          <div className="p-1 absolute left-0 top-11 opacity-0 invisible transition-colors group-hover:opacity-100 group-hover:visible z-10">
-            <ul className="row shadow bg-base-200 gap-3 py-3 px-6 rounded-box">
-              <li>
-                <Link href={""}>LinkedIn</Link>
-              </li>
-              <li>
-                <Link href={""}>Instagram</Link>
-              </li>
-              <li>
-                <Link href={""}>Telegram</Link>
-              </li>
-              <li>
-                <Link href={""}>Github</Link>
-              </li>
-              <li>
-                <Link href={""}>Email</Link>
-              </li>
-              <li>
-                <Link href={""}>Phone</Link>
-              </li>
-            </ul>
+
+        <div className="dropdown mr-5" dir="ltr">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <ShareIcon tabIndex={0} className="icon" />
           </div>
-        </button>
-        <label className="swap swap-rotate btn btn-ghost btn-circle mr-3">
-          <input type="checkbox" className="theme-controller" value="synthwave" />
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-300 shadow p-2 rounded-box z-10"
+          >
+            <li>
+              <a>Linkedin</a>
+            </li>
+            <li>
+              <a>Instagram</a>
+            </li>
+            <li>
+              <a>Telegram</a>
+            </li>
+            <li>
+              <a>Github</a>
+            </li>
+            <li>
+              <a>farzad.vav.work@gmail.com</a>
+            </li>
+            <li>
+              <a>09389461065</a>
+            </li>
+          </ul>
+        </div>
+        <label className="btn btn-ghost btn-circle swap swap-rotate mr-3">
+          <input
+            type="checkbox"
+            className="theme-controller"
+            defaultChecked={localStorage.getItem("theme") === "light"}
+            onChange={(e) => themeToggleHandler(e.target.checked ? "light" : "dark")}
+          />
           <SunIcon className="swap-off icon" />
           <MoonIcon className="swap-on icon" />
         </label>
