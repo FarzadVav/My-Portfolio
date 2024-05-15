89143dee9075e1a407730627ad8382a87a16887c
FarzadVav <farzad.vav.work@gmail.com>
2024-05-15 16:46:31 +0330

    add: icons to hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,66 @@ import React from "react"
 const Page = () => {
   return (
     <>
-      <section className="my-element row flex-col w-full xl:my-28">
+      <section className="my-element row flex-col w-full relative xl:my-28">
+        {/* icons */}
+        <div className="hero_icons-hover w-16 h-16 rounded-full absolute -top-10 left-96">
+          <Image
+            className="rounded-full"
+            width={64}
+            height={64}
+            src={"/icons/react.png"}
+            alt="react"
+          />
+        </div>
+        <div className="hero_icons-hover w-32 h-32 rounded-full absolute -top-10 left-20">
+          <Image className="rounded-full" width={128} height={128} src={"/icons/ts.png"} alt="ts" />
+        </div>
+        <div className="hero_icons-hover w-24 h-24 rounded-full absolute top-32 left-60">
+          <Image
+            className="rounded-full"
+            width={96}
+            height={96}
+            src={"/icons/next.png"}
+            alt="next"
+          />
+        </div>
+        <div className="hero_icons-hover w-16 h-16 rounded-full absolute -top-10 right-96">
+          <Image
+            className="rounded-full"
+            width={64}
+            height={64}
+            src={"/icons/tailwind.png"}
+            alt="tailwind"
+          />
+        </div>
+        <div className="hero_icons-hover w-32 h-32 rounded-full absolute -top-10 right-20">
+          <Image className="rounded-full" width={128} height={128} src={"/icons/js.png"} alt="js" />
+        </div>
+        <div className="hero_icons-hover w-24 h-24 rounded-full absolute top-32 right-60">
+          <Image
+            className="rounded-full"
+            width={128}
+            height={128}
+            src={"/icons/mysql.png"}
+            alt="mysql"
+          />
+        </div>
+        {/* icons */}
         <div className="w-60 h-60 relative">
+          {/* blured */}
           <div className="blured-box bg-secondary top-0 left-0"></div>
           <div className="blured-box bg-primary top-1/3 righ-0 -translate-y-1/2"></div>
           <div className="blured-box bg-info top-60 left-1/2 -translate-y-full -translate-x-1/2"></div>
+          {/* blured */}
           <Image
-            className="bg-neutral rounded-full relative z-10"
+            className="bg-neutral ring-8 ring-base-100 rounded-full relative z-10"
             width={240}
             height={240}
-            src={""}
+            src={"/images/profile.jpg"}
             alt=""
           />
         </div>
-        <h1 className="mt-element title-xl block w-3/4 text-center">
+        <h1 className="mt-element title-xl block w-3/4 text-center xl:mt-16">
           فرزاد هستم برنامه نویس
           <p className="text-primary inline-block text-6xl mx-2">
             <span>Next.js</span>
