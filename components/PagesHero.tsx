1b9c4c146f76c5848f6173c7b21fadb2b37e76e1
FarzadVav <farzad.vav.work@gmail.com>
2024-05-22 16:57:22 +0330

    add: PagesHero component for must top elements in all routes

---
+++ components/PagesHero.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import Image from "next/image"
+import { motion } from "framer-motion"
+import { ArrowDownLeftIcon } from "@heroicons/react/24/outline"
+import { v4 } from "uuid"
+
+import cn from "@/lib/cn"
+
+type PagesHeroT = {
+  className?: string
+  picture: string
+  title: string
+  text: string
+  links: { name: string; href: string }[]
+}
+
+const PagesHero = ({ className, picture, title, text, links }: PagesHeroT) => {
+  return (
+    <div className={cn("container my-element", className)}>
+      <div className="center max-w-[65%] mx-auto">
+        <motion.div
+          className="size-40 min-w-40 rounded-full"
+          initial={{ opacity: 0, scale: 0.5, rotate: 15 }}
+          animate={{ opacity: 1, scale: 1, rotate: 0 }}
+        >
+          <Image src={picture} width={160} height={160} alt={title} />
+        </motion.div>
+        <motion.div className="mr-9" initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }}>
+          <h1 className="title-lg">{title}</h1>
+          <p className="text-base-content/75 leading-relaxed mt-2.5">{text}</p>
+          <div className="row justify-end gap-6 mt-4">
+            {links.map((link) => (
+              <a key={v4()} className="link-hover row" href={link.href}>
+                <span>{link.name}</span>
+                <ArrowDownLeftIcon className="icon-sm mr-2" />
+              </a>
+            ))}
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  )
+}
+
+export default PagesHero
