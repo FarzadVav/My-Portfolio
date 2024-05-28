df8c69573f174f86a3d7913a5164208210f5e49c
FarzadVav <farzad.vav.work@gmail.com>
2024-05-28 18:54:13 +0330

    add: replay section to ArticleComment component

--- components/ArticleComment.tsx
+++ components/ArticleComment.tsx
@@ -31,6 +31,62 @@ const ArticleComment = ({ username, date, score, text, like, dislike }: ArticleC
         </div>
       </div>
       <p className="w-full text-justify mt-3">{text}</p>
+      {/* Replay */}
+      <div className="w-full" id="replay">
+        <div className="bg-base-100 w-full p-3 mt-6 rounded-box">
+          <div className="row w-full">
+            <div className="bg-base-300 size-12 rounded-full"></div>
+            <span className="font-base-bold text-xl mr-3">{username}</span>
+            <span className="opacity-75 mx-3">-</span>
+            <span className="italic">{new Date(date).toLocaleDateString("fa-ir")}</span>
+            <span className="opacity-50 italic mr-auto">در پاسخ به قبلی</span>
+          </div>
+          <p className="w-full text-justify mt-3">{text}</p>
+          <div className="row border-t border-light w-full pt-6 mt-6">
+            <button className="btn">
+              <span>پاسخ</span>
+              <ArrowUturnLeftIcon className="icon" />
+            </button>
+            <div className="row gap-1.5 text-sm mr-auto">
+              <button className="btn btn-ghost btn-circle">
+                <HandThumbUpIcon className="icon" />
+              </button>
+              <span>{like}</span>
+              <button className="btn btn-ghost btn-circle mr-1.5">
+                <HandThumbDownIcon className="icon" />
+              </button>
+              <span>{dislike}</span>
+            </div>
+          </div>
+        </div>
+        <div className="bg-base-100 w-full p-3 mt-6 rounded-box">
+          <div className="row w-full">
+            <div className="bg-base-300 size-12 rounded-full"></div>
+            <span className="font-base-bold text-xl mr-3">{username}</span>
+            <span className="opacity-75 mx-3">-</span>
+            <span className="italic">{new Date(date).toLocaleDateString("fa-ir")}</span>
+            <span className="opacity-50 italic mr-auto">در پاسخ به قبلی</span>
+          </div>
+          <p className="w-full text-justify mt-3">{text}</p>
+          <div className="row border-t border-light w-full pt-6 mt-6">
+            <button className="btn">
+              <span>پاسخ</span>
+              <ArrowUturnLeftIcon className="icon" />
+            </button>
+            <div className="row gap-1.5 text-sm mr-auto">
+              <button className="btn btn-ghost btn-circle">
+                <HandThumbUpIcon className="icon" />
+              </button>
+              <span>{like}</span>
+              <button className="btn btn-ghost btn-circle mr-1.5">
+                <HandThumbDownIcon className="icon" />
+              </button>
+              <span>{dislike}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Replay */}
       <div className="row border-t border-light w-full pt-6 mt-6">
         <button className="btn">
           <span>پاسخ</span>
