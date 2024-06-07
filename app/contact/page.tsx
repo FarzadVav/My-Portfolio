f216c14997d24a6ff1494dbe40cfc0c1286ff1a0
FarzadVav <farzad.vav.work@gmail.com>
2024-06-07 14:48:06 +0330

    add: responsive styles to /contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -31,17 +31,17 @@ const Page = () => {
       />
 
       {showForm ? (
-        <form className="max-w-4xl mx-auto">
-          <div className="row">
-            <input type="text" placeholder="نام" className="input input-bordered w-1/2" />
+        <form className="container mx-auto lg:max-w-4xl">
+          <div className="row max-sm:flex-col">
+            <input type="text" placeholder="نام" className="input input-bordered w-full sm:w-1/2" />
             <input
               type="text"
               placeholder="نام خانوادگی"
-              className="input input-bordered w-1/2 mr-3"
+              className="input input-bordered w-full sm:w-1/2 sm:mr-3 max-sm:mt-3"
             />
           </div>
-          <div className="row mt-3">
-            <select className="select select-bordered w-1/2 pr-4">
+          <div className="row mt-3 max-sm:flex-col-reverse">
+            <select className="select select-bordered w-full pr-4 sm:w-1/2 max-sm:mt-3">
               <option disabled selected>
                 موضوع پیام را انتخاب کنید
               </option>
@@ -52,7 +52,7 @@ const Page = () => {
             <input
               type="text"
               placeholder="example@gmail.com"
-              className="input input-bordered w-1/2 mr-3"
+              className="input input-bordered w-full sm:w-1/2 sm:mr-3"
               dir="ltr"
             />
           </div>
@@ -63,12 +63,12 @@ const Page = () => {
           <button className="btn btn-primary mt-6">ارسال پیام</button>
         </form>
       ) : (
-        <div className="max-w-4xl mx-auto">
+        <div className="container mx-auto lg:max-w-4xl">
           <header className="row bg-base-300 w-full py-3 px-5 rounded-t-box">
             <button className="btn btn-ghost btn-circle">
               <ShareIcon className="icon" />
             </button>
-            <span className="font-base-bold text-lg mr-3">فرزاد وحدتی نژاد</span>
+            <span className="font-base-bold mr-3 sm:text-lg">فرزاد وحدتی نژاد</span>
             <Image
               className="size-12 mr-auto rounded-full"
               src={"/images/profile.jpg"}
@@ -77,42 +77,96 @@ const Page = () => {
               alt="پروفایل ادمین"
             />
           </header>
-          <main className="bg-base-200 w-full min-h-24 max-h-96 py-3 px-5">
+          <main className="bg-base-200 w-full min-h-24 max-h-[65vh] py-3 px-5 overflow-y-auto">
             <div className="chat chat-start">
               <div className="chat-header pb-1">
                 <time className="text-xs">12:45</time>
               </div>
-              <div className="chat-bubble chat-bubble-primary">
+              <div className="chat-bubble chat-bubble-primary max-sm:text-sm">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
                 گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای
                 شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
                 باشد
               </div>
-              <div className="chat-footer opacity-50 pt-1">دیده شده</div>
+              <div className="chat-footer opacity-50 pt-1 max-sm:text-xs">دیده شده</div>
             </div>
             <div className="chat chat-end">
               <div className="chat-header pb-1">
                 <time className="text-xs">12:45</time>
               </div>
-              <div className="chat-bubble">تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</div>
+              <div className="chat-bubble max-sm:text-sm">تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-header pb-1">
+                <time className="text-xs">12:45</time>
+              </div>
+              <div className="chat-bubble chat-bubble-primary max-sm:text-sm">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای
+                شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
+                باشد
+              </div>
+              <div className="chat-footer opacity-50 pt-1 max-sm:text-xs">دیده شده</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-header pb-1">
+                <time className="text-xs">12:45</time>
+              </div>
+              <div className="chat-bubble max-sm:text-sm">تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-header pb-1">
+                <time className="text-xs">12:45</time>
+              </div>
+              <div className="chat-bubble chat-bubble-primary max-sm:text-sm">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای
+                شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
+                باشد
+              </div>
+              <div className="chat-footer opacity-50 pt-1 max-sm:text-xs">دیده شده</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-header pb-1">
+                <time className="text-xs">12:45</time>
+              </div>
+              <div className="chat-bubble max-sm:text-sm">تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-header pb-1">
+                <time className="text-xs">12:45</time>
+              </div>
+              <div className="chat-bubble chat-bubble-primary max-sm:text-sm">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+                گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای
+                شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می
+                باشد
+              </div>
+              <div className="chat-footer opacity-50 pt-1 max-sm:text-xs">دیده شده</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-header pb-1">
+                <time className="text-xs">12:45</time>
+              </div>
+              <div className="chat-bubble max-sm:text-sm">تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود</div>
             </div>
           </main>
-          <footer className="row bg-base-300 w-full py-3 px-5 rounded-b-box">
-            <button className="btn btn-primary btn-circle">
-              <PaperAirplaneIcon className="icon -rotate-45" />
+          <footer className="row bg-base-300 w-full p-1.5 rounded-b-box sm:py-3 sm:px-5">
+            <button className="btn btn-primary btn-circle max-sm:btn-sm">
+              <PaperAirplaneIcon className="icon-sm -rotate-45 sm:icon" />
             </button>
             <input
               type="text"
               placeholder="پیامی تایپ کنید ..."
-              className="input input-bordered bg-base-300 flex-1 mr-3"
+              className="input input-bordered bg-base-300 flex-1 mr-3 max-sm:input-sm"
             />
-            <button className="btn btn-ghost btn-circle mr-3">
-              <DocumentIcon className="icon" />
+            <button className="btn btn-ghost btn-circle mr-1.5 sm:mr-3 max-sm:btn-sm">
+              <DocumentIcon className="icon-sm sm:icon" />
             </button>
-            <div className="dropdown dropdown-top mr-1.5">
-              <div tabIndex={0} role="button" className="btn btn-ghost">
-                <span>دسته بندی</span>
-                <ChevronDownIcon tabIndex={0} className="icon" />
+            <div className="dropdown dropdown-top dropdown-end sm:mr-1.5">
+              <div tabIndex={0} role="button" className="btn btn-ghost max-sm:btn-sm">
+                <span className="max-md:hidden">دسته بندی</span>
+                <ChevronDownIcon tabIndex={0} className="icon-sm sm:icon" />
               </div>
               <ul
                 tabIndex={0}
@@ -145,7 +199,7 @@ const Page = () => {
         </TitleAnimaiton>
         <div className="collapse collapse-arrow mt-element bg-base-200">
           <input type="radio" name="my-accordion-2" defaultChecked />
-          <span className="collapse-title font-base-bold text-xl">
+          <span className="collapse-title font-base-bold text-lg md:text-xl">
             چگونه در 3 روز دیتابیس دیجی کالا را هک کنیم؟
           </span>
           <div className="collapse-content">
@@ -154,7 +208,7 @@ const Page = () => {
         </div>
         <div className="collapse collapse-arrow bg-base-200 mt-3">
           <input type="radio" name="my-accordion-2" />
-          <span className="collapse-title font-base-bold text-xl">
+          <span className="collapse-title font-base-bold text-lg md:text-xl">
             چگونه در 3 روز دیتابیس دیجی کالا را هک کنیم؟
           </span>
           <div className="collapse-content">
@@ -163,7 +217,7 @@ const Page = () => {
         </div>
         <div className="collapse collapse-arrow bg-base-200 mt-3">
           <input type="radio" name="my-accordion-2" />
-          <span className="collapse-title font-base-bold text-xl">
+          <span className="collapse-title font-base-bold text-lg md:text-xl">
             چگونه در 3 روز دیتابیس دیجی کالا را هک کنیم؟
           </span>
           <div className="collapse-content">
