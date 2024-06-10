42e85939ad59f00c55632657eab1da0c095ac93d
FarzadVav <farzad.vav.work@gmail.com>
2024-06-10 18:23:19 +0330

    fix: Tags direction

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { HashtagIcon } from "@heroicons/react/24/outline"
+
 import cn from "@/lib/cn"
 
 type TagT = {
@@ -9,9 +10,13 @@ type TagT = {
 
 const Tag = ({ className, name }: TagT) => {
   return (
-    <Link className={cn("btn rounded-full max-sm:btn-sm max-sm:text-sm", className)} href={`/articles?tag=${name}`}>
-      <span>{name}</span>
+    <Link
+      className={cn("btn rounded-full max-sm:btn-sm max-sm:text-sm", className)}
+      href={`/articles?tag=${name}`}
+      dir="ltr"
+    >
       <HashtagIcon className="icon-xs sm:icon-sm" />
+      <span>{name}</span>
     </Link>
   )
 }
