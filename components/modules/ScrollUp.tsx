b02a3b3c25d687ee2aaedf26dd311786219312bb
FarzadVav <farzad.vav.work@gmail.com>
2024-07-21 12:53:28 +0330

    add: framer-motion animations to ScrollUp component

--- components/modules/ScrollUp.tsx
+++ components/modules/ScrollUp.tsx
@@ -1,12 +1,19 @@
 "use client"
 
+import { motion } from "framer-motion"
 import { ArrowUpIcon } from "@heroicons/react/24/outline"
 
 const ScrollUp = () => {
   return (
-    <button className="btn btn-circle" onClick={() => window.scrollTo(0, 0)}>
+    <motion.button
+      initial={{ scale: 0, y: 50 }}
+      whileInView={{ scale: 1, y: 0 }}
+      viewport={{ once: true }}
+      className="btn btn-circle"
+      onClick={() => window.scrollTo(0, 0)}
+    >
       <ArrowUpIcon className="icon" />
-    </button>
+    </motion.button>
   )
 }
 
