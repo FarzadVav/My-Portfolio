9c852c4b2b528ce3e5cbefdc52d44ba3c61d9815
FarzadVav <farzad.vav.work@gmail.com>
2024-08-08 13:48:56 +0330

    fix: PagesHero

--- components/PagesHero.tsx
+++ components/PagesHero.tsx
@@ -29,61 +29,53 @@ const PagesHero = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
       <div className={cn("w-full my-element", className)} {...props}>
         <div className="container center mx-auto max-md:flex-col lg:max-w-4xl">
           {isLoading ? (
-            <div className="skeleton size-40 min-w-40"></div>
+            <div className="skeleton size-[184px]"></div>
           ) : (
             <motion.div
-              className="size-40 min-w-40 rounded-box"
+              className="size-[184px] rounded-box"
               initial={{ opacity: 0, scale: 0.25, y: 25, rotate: 25 }}
               animate={{ opacity: 1, scale: 1, y: 0, rotate: 0 }}
             >
               <Image
                 className="bg-base-200 rounded-box"
                 src={page?.data?.picture || ""}
-                width={160}
-                height={160}
+                width={184}
+                height={184}
                 alt={page?.data?.name || ""}
                 priority
               />
             </motion.div>
           )}
           {isLoading ? (
-            <div className="w-full h-[260.4px] flex flex-col justify-between sm:h-[182.4px] md:w-[calc(100%-10rem)] md:h-[170.4px] md:mr-9 max-md:items-center lg:h-[172px] xl:h-[176px]">
-              <div className="skeleton w-1/2 h-7 max-md:mt-3"></div>
+            <div className="w-full h-[190.4px] md:h-[178.4px] lg:h-[184px] flex flex-col justify-between py-3 md:w-[calc(100%-184px)] md:pr-9">
+              <div className="skeleton w-1/2 h-6 max-md:mx-auto max-md:mt-3"></div>
               <div className="skeleton w-full h-2"></div>
               <div className="skeleton w-full h-2"></div>
               <div className="skeleton w-full h-2"></div>
               <div className="skeleton w-full h-2"></div>
-              <div className="skeleton w-full h-2"></div>
-              <div className="skeleton w-full h-2"></div>
-              <div className="skeleton w-full h-2 sm:hidden"></div>
-              <div className="skeleton w-full h-2 sm:hidden"></div>
-              <div className="skeleton w-1/2 lg:w-1/3 h-2 md:mr-auto max-md:ml-auto"></div>
+              <div className="row w-full">
+                <div className="skeleton w-24 h-2"></div>
+                <div className="skeleton w-1/2 h-2 mr-auto"></div>
+              </div>
             </div>
           ) : (
             <motion.div
-              className="w-full md:w-[calc(100%-10rem)] md:mr-9"
+              className="w-full md:w-[calc(100%-184px)] md:pr-9"
               initial={{ opacity: 0, x: 50 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ delay: 0.05 }}
             >
               <h1 className="title-base max-md:center max-md:mt-3">{page?.data?.title}</h1>
-              <div className="row w-full h-[156px] mt-3 overflow-hidden sm:h-[78px]">
-                <p className="content-text w-full max-h-full text-justify relative">
-                  {page?.data?.description.slice(0, 220)}
-                  {(page?.data?.description.length || 0) >= 220 ? (
-                    <>
-                      <span className="inline-block">...</span>
-                      <button
-                        className="btn btn-xs btn-link text-base-content inline-block"
-                        onClick={() => showModal(moodalId)}
-                      >
-                        دیدن کامل
-                      </button>
-                    </>
-                  ) : null}
-                </p>
-              </div>
-              <div className="row w-full gap-x-6 pb-1.5 mt-4 overflow-x-auto md:justify-end">
+              <p className="content-text w-full h-[78px] text-justify line-clamp-3 mt-3">
+                {page?.data?.description}
+              </p>
+              <div className="row w-full gap-x-6 pb-1.5 mt-4 overflow-x-auto">
+                <button
+                  className="btn btn-sm min-w-max max-w-max flex-1 ml-auto"
+                  onClick={() => showModal(moodalId)}
+                >
+                  دیدن کامل
+                </button>
                 {page?.data?.sections.map((section) => (
                   <Link
                     key={v4()}
