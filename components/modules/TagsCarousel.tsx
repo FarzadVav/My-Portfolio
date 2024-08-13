b00c6a0dd69dca8941f39928a4b50ca6cbfd652d
FarzadVav <farzad.vav.work@gmail.com>
2024-08-13 12:54:52 +0330

    fix: TagsCarousel animation

--- components/modules/TagsCarousel.tsx
+++ components/modules/TagsCarousel.tsx
@@ -7,18 +7,15 @@ type TagsCarouselT = {
 
 const TagsCarousel = ({ tags }: TagsCarouselT) => {
   return (
-    <div className="mt-element w-full h-12 relative overflow-hidden group">
-      <ul className="tags-animation row min-w-full absolute top-0 right-0 group-hover:animation-pause">
+    <div className="mt-element row w-full h-12 relative overflow-hidden group">
+      <ul className="tags-animation">
         {tags.map((tag) => (
-          <li className="center min-w-max flex-1 ml-1.5" key={tag.id}>
+          <li key={tag.id}>
             <Tag name={tag.name} />
           </li>
         ))}
       </ul>
-      <ul
-        style={{ animationDelay: "20s" }}
-        className="tags-animation row min-w-full absolute top-0 right-0 group-hover:animation-pause"
-      >
+      <ul className="tags-animation">
         {tags.map((tag) => (
           <li className="center min-w-max flex-1 ml-1.5" key={tag.id}>
             <Tag name={tag.name} />
