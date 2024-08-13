65d10ce38a9b77de0fa186a6322ccb1f35efb0b5
FarzadVav <farzad.vav.work@gmail.com>
2024-08-13 11:21:15 +0330

    fix: ArticleCategory styles

--- components/ArticleCategory.tsx
+++ components/ArticleCategory.tsx
@@ -15,11 +15,11 @@ type ArticleCategoryT = HTMLAttributes<HTMLElement> & ArticlesCategoriesT
 const ArticleCategory = ({ className, id, name, logo, articles, tags }: ArticleCategoryT) => {
   return (
     <article className={cn("rounded-box", className)}>
-      <div className="bg-base-300 w-full h-24 pt-4 rounded-t-box relative">
+      <div className="bg-base-300 w-full h-28 pt-4 rounded-t-box relative">
         {/* rounded absolute element */}
         <div className="bg-base-200 w-1/2 h-4 absolute top-0 left-0 rounded-t-box sm:w-1/3"></div>
         {/* rounded absolute element */}
-        <div className="bg-base-200 w-full h-20 px-6 rounded-tr-box relative">
+        <div className="bg-base-200 w-full h-24 px-6 rounded-tr-box relative">
           <Cards logo={logo} />
         </div>
       </div>
@@ -34,11 +34,11 @@ const ArticleCategory = ({ className, id, name, logo, articles, tags }: ArticleC
         </Link>
 
         <div className="row w-full mt-6">
-          <p className="font-bold row leading-relaxed">
+          <p className="font-bold row leading-relaxed" title="تعداد مقالات">
             <DocumentDuplicateIcon className="icon" />
             <span className="mr-1.5">{articles.length}</span>
           </p>
-          <p className="font-bold row leading-relaxed mr-3">
+          <p className="font-bold row leading-relaxed mr-3" title="تعداد هشتگ ها">
             <HashtagIcon className="icon" />
             <span className="mr-1.5">{tags.length}</span>
           </p>
@@ -51,26 +51,26 @@ const ArticleCategory = ({ className, id, name, logo, articles, tags }: ArticleC
 const Cards = ({ logo }: { logo: string }) => {
   return (
     <>
-      <div className="bg-base-100 shadow-xl shadow-base-300 w-1/3 h-16 p-3 rounded-t-box -rotate-6 absolute -bottom-2.5 left-6">
+      <div className="bg-base-100 shadow-lg shadow-base-300 w-1/3 h-[4.5rem] p-3 rounded-t-box -rotate-6 absolute -bottom-2.5 left-6">
         <span className="bg-base-content/10 block h-2 rounded-full"></span>
         <span className="bg-base-content/10 block h-2 mt-1 rounded-full"></span>
         <span className="bg-base-content/10 block h-2 mt-1 rounded-full"></span>
         <span className="bg-base-content/10 block h-2 mt-1 rounded-full"></span>
       </div>
 
-      <div className="bg-base-100 shadow-xl shadow-base-300 w-1/3 h-12 p-3 rounded-t-box absolute left-1/2 bottom-0 -translate-x-1/2">
-        <span className="bg-base-content/10 block w-1/3 h-2 rounded-full"></span>
-        <span className="bg-base-content/10 block w-full h-1 mt-2 rounded-full"></span>
+      <div className="bg-base-100 shadow-lg shadow-base-300 w-1/3 h-12 p-3 rounded-t-box absolute left-1/2 bottom-0 -translate-x-2/3" dir="ltr">
+        <span className="bg-base-content/10 block w-1/3 h-1 mt-1 rounded-full"></span>
+        <span className="bg-base-content/10 block w-full h-1 mt-1 rounded-full"></span>
         <span className="bg-base-content/10 block w-full h-1 mt-1 rounded-full"></span>
       </div>
 
-      <div className="bg-base-100 shadow-xl shadow-base-300 w-1/3 min-w-[100px] h-16 p-3 rounded-t-box absolute bottom-0 right-6">
+      <div className="bg-base-100 shadow-lg shadow-base-300 w-1/2 min-w-[100px] h-20 p-3 rounded-t-box absolute bottom-0 right-6">
         <div className="row">
           <Image
-            className="size-7 rounded-full"
+            className="ring-4 ring-base-content/10 size-10 rounded-full sm:size-12"
             src={logo}
-            width={28}
-            height={28}
+            width={48}
+            height={48}
             alt={"لوگوی دسته بندی"}
           />
           <div className="w-1/2 mr-auto">
